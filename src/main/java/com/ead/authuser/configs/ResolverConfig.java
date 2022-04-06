24aad2176efe8d335b9e47d3c935ef6a5cdd9585
MuriloAbranches <muriloads@hotmail.com>
2022-04-05 23:13:44 -0300

    Correções e atualizações

--- src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Configuration
 public class ResolverConfig extends WebMvcConfigurationSupport {
 
+    @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new SpecificationArgumentResolver());
 
